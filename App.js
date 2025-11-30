95e1505a305ce63bcac0d48f07e16feda7329073
debelique <n.m.marov@gmail.com>
2025-11-30 14:17:58 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const URL = "https://raw.githubusercontent.com/debelique/solarpanelcalculator_cs385/refs/heads/main/PV_Module_List_Full_Data_ADAmicro.json";
+
+    async function fetchData() {
+      try {
+        const response = await fetch(URL);
+        const dataJson = await response.json();
+        setLoading(true);
+        setData(dataJson);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []); // end of useEffect
+
+  if (error) {
+    return <h1>Opps! An error has occurred: {error.toString()}</h1>;
+  } else if (loading === false) {
+    return <h1>Waiting for the data .....</h1>;
+  } else {
+    return (
+      <>
+        <SolarFormComponent APIData={data} />
+      </>
+    );
+  }
+} // end App() function or component
+
+function SolarFormComponent(props) {
+    const [kWhMonth, setkWhMonth] = useState(375);
+    
+    return (
+        <>
+            <h1>Solar Net Zero Calculator</h1>
+            <p>How much panels do you need to disconnec from the grid</p>
+            <p>
+                <label>How much kW/h do you use per month:</label>
+                <input id="kWhMonth" type="text" value={kWhMonth}/>
+            </p>
+            <PanelDropDownComponent APIData={props.APIData} />
+            <p>
+                <label> Enter season: </label>
+                <select id="Season">
+                    <option value="Summer"> Summer </option>
+                    <option value="Winter"> Winter </option> 
+                </select>
+            </p>
+            <p><button>Calculate</button></p>
+        </>
+      );
+}
+
+function PanelDropDownComponent(props) {
+    const [selectedPanel, setSelectedPanel] = useState("");   
+    const handleChange = (event) => {
+        setSelectedPanel(event.target.value);
+    };
+    const selectedPanel_var = props.APIData.find((element) => element.Id == selectedPanel);
+
+    return (
+        <>
+            <p>
+                    <label>Type of panel:</label>
+                    <select onChange={handleChange} id="panel">
+                        {props.APIData.map((t, index) => (
+                             <option value={t.Id}>{t.ModelNumber}</option>
+                         ))}
+                    </select>
+            </p>
+            <PanelDetailsComponent data={selectedPanel_var}/>
+         </>
+    );
+}
+
+function PanelDetailsComponent(props) {
+    //console.log("value >>", props);
+    if (props.data)
+    {
+        return (
+            <>
+             <p><label>Manufacturer:</label> <span>{props.data.Manufacturer}</span></p>
+            <p><label>Description:</label> <span>{props.data.Description}</span></p>
+            <p><label>Power:</label> <span>{props.data.NameplatePmax}</span></p>
+            <p><label>Voltage:</label> <span>{props.data.NameplateVpmax}</span></p>               
+            </> 
+        );
+    }
+}
+
+export default App;
+
